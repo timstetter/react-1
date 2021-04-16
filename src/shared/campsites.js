c04879b372a0a7f6fa022cf489dd195f3a265e83
timstetter <tstetter@gmail.com>
2021-04-15 22:55:42 -0400

    adding data to single page app

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -1,190 +1,34 @@
 export const CAMPSITES = [
-  {
-    id: 0,
-    name: "React Lake Campground",
-    image: "assets/images/react-lake.jpg",
-    elevation: 1233,
-    description:
-      "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
-    comments: [
-      {
+    {
         id: 0,
-        rating: 5,
-        text: "What a magnificent view!",
-        author: "Tinus Lorvaldes",
-        date: "2018-10-25T16:30Z",
-      },
-      {
+        name: "React Lake Campground",
+        image: "/assets/images/react-lake.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+    },
+    {
         id: 1,
-        rating: 4,
-        text:
-          "The campground was beautiful, but the bugs could bite sometimes.",
-        author: "Brennen Ech",
-        date: "2017-06-17T03:33Z",
-      },
-      {
+        name:"Chrome River Campground ",
+        image: "/assets/images/chrome-river.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+    },
+    {
         id: 2,
-        rating: 5,
-        text: "I caught a world-record-breaking trout here on my last visit.",
-        author: "McKenzie Sebastian",
-        date: "2019-02-18T18:12Z",
-      },
-      {
+        name:"Breadcrumb Trail Campground",
+        image: "/assets/images/breadcrumb-trail.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+    },
+    {
         id: 3,
-        rating: 5,
-        text: "A wonderful place to reconnect with nature.",
-        author: "Jordan Runn",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "The stars at night were magnificent!",
-        author: "Ann Dabramov",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
-  {
-    id: 1,
-    name: "Chrome River Campground ",
-    image: "assets/images/chrome-river.jpg",
-    elevation: 877,
-    description:
-      "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
-    comments: [
-      {
-        id: 0,
-        rating: 5,
-        text: "You miss 100% of the shots you don’t take. - Wayne Gretzky",
-        author: "Michael Scott",
-        date: "2018-10-25T16:30Z",
-      },
-      {
-        id: 1,
-        rating: 4,
-        text:
-          "Whenever I'm about to do something, I think 'Would an idiot do that?' And if they would, I do not do that thing.",
-        author: "Dwight Schrute",
-        date: "2017-06-17T03:33Z",
-      },
-      {
-        id: 2,
-        rating: 5,
-        text: "Boy Have You Lost Your Mind, Cause I'll Help You Find It.",
-        author: "Stanley Hudson",
-        date: "2019-02-18T18:12Z",
-      },
-      {
-        id: 3,
-        rating: 5,
-        text:
-          "I Just Want To Eat, Which I Realize Is A Lot To Ask, At A Dinner Party.",
-        author: "Pam Halpert",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "That's Really Not The Trend In Vampires Right Now",
-        author: "Ryan Howard",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Breadcrumb Trail Campground",
-    image: "assets/images/breadcrumb-trail.jpg",
-    elevation: 2901,
-    description:
-      "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
-    comments: [
-      {
-        id: 0,
-        rating: 5,
-        text:
-          "We’re Building Something, Here, Detective, We’re Building It From Scratch. All The Pieces Matter.",
-        author: "Lester Freamon",
-        date: "2018-10-25T16:30Z",
-      },
-      {
-        id: 1,
-        rating: 4,
-        text: "You Come At The King, You Best Not Miss.",
-        author: "Omar Little",
-        date: "2017-06-17T03:33Z",
-      },
-      {
-        id: 2,
-        rating: 5,
-        text:
-          "Well, You Know What They Say: ‘Stupid Criminal Make Stupid Cops.’ I’m Proud To Be Chasing This Guy",
-        author: "Jimmy McNulty",
-        date: "2019-02-18T18:12Z",
-      },
-      {
-        id: 3,
-        rating: 5,
-        text:
-          "You Know The Difference Between Me And You? I Bleed Red And You Bleed Green",
-        author: "Avon Barksdale",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "We Ain’t Gotta Dream No More, Man.",
-        author: "Russell 'Stringer' Bell",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Redux Woods Campground",
-    image: "assets/images/redux-woods.jpg",
-    elevation: 42,
-    description:
-      "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
-    comments: [
-      {
-        id: 0,
-        rating: 5,
-        text: "Logic is the beginning of wisdom, not the end.",
-        author: "Spock",
-        date: "2018-10-25T16:30Z",
-      },
-      {
-        id: 1,
-        rating: 4,
-        text:
-          "Compassion: that's the one things no machine ever had. Maybe it's the one thing that keeps men ahead of them.",
-        author: "Dr. McCoy",
-        date: "2017-06-17T03:33Z",
-      },
-      {
-        id: 2,
-        rating: 5,
-        text:
-          "It is possible to commit no errors and still lose. That is not a weakness. That is life.",
-        author: "Jean-Luc Picard",
-        date: "2019-02-18T18:12Z",
-      },
-      {
-        id: 3,
-        rating: 5,
-        text: "To boldly go where no man has gone before.",
-        author: "James T. Kirk",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "I canna' change the laws of physics.",
-        author: 'Montgomery "Scotty" Scott',
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
+        name:"Redux Woods Campground",
+        image: "/assets/images/redux-woods.jpg",
+        elevation: 42,
+        featured: true,
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
 ];
