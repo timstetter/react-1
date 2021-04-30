7c43c06d5afdf9956f7a1bfe33d5d36b418c36ca
timstetter <tstetter@gmail.com>
2021-04-30 12:58:33 -0400

    fixed value in select

--- src/components/CampsiteInfoComponent.jsx
+++ src/components/CampsiteInfoComponent.jsx
@@ -100,14 +100,14 @@ class CommentForm extends Component {
                   validators={{
                     required,
                   }}>
-                  <option value="" selected disabled>
+                  <option selected disabled>
                     Choose here
                   </option>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
                 </Control.select>
                 <Errors
                   className="text-danger"
